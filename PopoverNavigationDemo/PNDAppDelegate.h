2e17f03ce398486dc0fb41b67cd7489c3a9cfe4a
Tim Ekl <lithium3141@gmail.com>
2013-02-12 10:32:22 -0800

    Initial Commit

---
+++ PopoverNavigationDemo/PNDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PNDAppDelegate.h
+//  PopoverNavigationDemo
+//
+//  Created by Tim Ekl on 2/12/13.
+//  Copyright (c) 2013 The Omni Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PNDAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PopoverNavigationDemo/PNDAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  PNDAppDelegate.m
+//  PopoverNavigationDemo
+//
+//  Created by Tim Ekl on 2/12/13.
+//  Copyright (c) 2013 The Omni Group. All rights reserved.
+//
+
+#import "PNDAppDelegate.h"
+
+@implementation PNDAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
